b6473943311f86065d49ff321003c81019261612
soeun135 <soensuckun@naver.com>
2023-09-27 06:35:31 +0900

    openweathermap API에서 데이터 가져오기 & 파싱 & DB에 저장 완료

---
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -0,0 +1,26 @@
+package zerobase.weather.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import zerobase.weather.service.DiaryService;
+
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    //날씨 일기 작성
+    @PostMapping("create/diary")
+    void createDiary(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestBody String text
+    ) {
+        diaryService.createDiary(date, text);
+    }
+}
---
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -0,0 +1,102 @@
+package zerobase.weather.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.repository.DiaryRepository;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class DiaryService {
+    private final DiaryRepository diaryRepository;
+    @Value("${openweathermap.key}")
+    private String apiKey;
+
+    public void createDiary(LocalDate date, String text) {
+        //open weather map에서 데이터 받아오기
+        String weatherData = getWeatherString();
+        //받아온 날씨 데이터 파싱하기
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
+        //파싱된 데이터 + 일기 값 우리 db에 저장
+        saveData(date, text, parsedWeather);
+    }
+
+    private void saveData(LocalDate date, String text, Map<String, Object> parsedWeather) {
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather((String) parsedWeather.get("main"));
+        nowDiary.setIcon((String) parsedWeather.get("icon"));
+        nowDiary.setTemperature((Double) parsedWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
+        diaryRepository.save(nowDiary);
+    }
+
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        //log.info(String.valueOf(jsonObject));
+        Map<String, Object> resultMap = new HashMap<>();
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", mainData.get("temp"));
+
+        JSONArray weatherData = (JSONArray) jsonObject.get("weather");
+        //log.info(String.valueOf(mainData));
+        JSONObject data = (JSONObject) weatherData.get(0);
+        resultMap.put("main", data.get("main"));
+        resultMap.put("icon", data.get("icon"));
+
+        log.info(resultMap.toString());
+
+        return resultMap;
+    }
+
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
+    }
+
+}
