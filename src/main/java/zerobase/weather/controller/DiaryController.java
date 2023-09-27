15f09c39eeba2235b35888cfb10d0bdd5799a7a6
soeun135 <soensuckun@naver.com>
2023-09-27 20:51:52 +0900

    간단한 예외처리

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.error.InvalidDate;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
@@ -25,6 +26,9 @@ public class DiaryController {
 
     @GetMapping("/read/diary")
     public List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+//        if(date.isAfter(LocalDate.ofYearDay(2023, 1))) {
+//            throw new InvalidDate();
+//        }
         return diaryService.readDiary(date);
     }
 
